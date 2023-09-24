a38328fab721505841c9a098e519faff41078044
Pawan Kumar Sharma <pawansharma19feb@gmail.com>
2023-09-24 20:24:04 +0530

    Added solution - LeetHub

--- Missing number in array - GFG/missing-number-in-array.cpp
+++ Missing number in array - GFG/missing-number-in-array.cpp
@@ -17,7 +17,7 @@ class Solution{
         int sum1=n*(n+1)/2;
     
         int sum2=0;
-        for(int i=0;i<n-1;i++){
+        for(int i=0;i<array.size();i++){
             sum2=sum2+array[i];
         }
         

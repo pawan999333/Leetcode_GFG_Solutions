8591a2824a9e7636f0e40be380c0e8e8ccfe96ca
Pawan Kumar Sharma <pawansharma19feb@gmail.com>
2024-04-17 16:14:59 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (82.63%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        
+        int XOR=0;
+        for(char &ch:s){
+            XOR^=ch;
+        }
+        for(char &ch:t){
+            XOR^=ch;
+        }
+        return (char)XOR;
+    }
+};

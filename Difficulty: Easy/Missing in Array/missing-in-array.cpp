48bc945db525140dcd863b44cc9ce6e8c958b37f
Pawan Kumar Sharma <pawansharma19feb@gmail.com>
2024-12-02 10:02:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Missing in Array/missing-in-array.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    int missingNumber(vector<int>& arr) {
+        // code here
+        
+        
+        int n=arr.size();
+        int sum=((n+1)*(n+2)/2);
+        int sum2=0;
+        for(int i=0;i<n;i++){
+            sum2+=arr[i];
+            
+        }
+        return (sum-sum2);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // to ignore the newline after the integer input
+    while (t--) {
+        int n;
+        vector<int> a;
+        string input;
+
+        // Input format: first number n followed by the array elements
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num);
+
+        Solution obj;
+        cout << obj.missingNumber(a) << endl;
+        cout << "~\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends

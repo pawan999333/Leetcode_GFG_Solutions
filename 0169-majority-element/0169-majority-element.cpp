bad1335e1f3b7ab66059dfe376db9c04dd600fc1
Pawan Kumar Sharma <pawansharma19feb@gmail.com>
2025-01-14 16:42:02 +0530

    Time: 3 ms (32.66%), Space: 28.2 MB (21.54%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -4,20 +4,37 @@ public:
 
        int n=nums.size();
     
-       for(int i=0;i<n-1;i++){
-        int ans=0;
-        for(int j=i+1;j<n;j++){
-            if(nums[i]==nums[j]){
-                ans++;
+    //    for(int i=0;i<n-1;i++){
+    //     int ans=0;
+    //     for(int j=i+1;j<n;j++){
+    //         if(nums[i]==nums[j]){
+    //             ans++;
+    //         }
+
+    //     }
+    //     if((ans+1)>n/2){
+    //         return nums[i];
+    //     }
+    //    }
+
+    //   return nums[0];
+
+
+    // better approach
+    int candidate=0,count=0;
+    for(int i=0;i<n;i++){
+        if(count==0){
+            count=1;
+            candidate=nums[i];
+        }else{
+            if(candidate==nums[i]){
+                count++;
+            }else{
+                count--;
             }
-
         }
-        if((ans+1)>n/2){
-            return nums[i];
-        }
-       }
-
-      return nums[0];
+    }
+    return candidate;
         
     }
 };

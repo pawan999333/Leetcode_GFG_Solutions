dd1734645063b15d523504897b84655e20a1e21b
Pawan Kumar Sharma <pawansharma19feb@gmail.com>
2025-03-02 17:26:52 +0530

    Time: 38 ms (93.39%), Space: 54.4 MB (6.56%) - LeetHub

---
+++ 2809-create-hello-world-function/2809-create-hello-world-function.js
@@ -0,0 +1,17 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return "Hello World";
+        
+    }
+};
+
+
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */

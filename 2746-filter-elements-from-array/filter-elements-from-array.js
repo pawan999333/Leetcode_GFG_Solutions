4855e8beb7d11ccbb871bb85bf33fa2a27cfc217
Pawan Kumar Sharma <pawansharma19feb@gmail.com>
2025-03-06 00:05:44 +0530

    Time: 38 ms (94.56%) | Memory: 53.2 MB (18.29%) - LeetSync

---
+++ 2746-filter-elements-from-array/filter-elements-from-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    let arr2=[];
+    for(let i=0;i<arr.length;i++){
+        if(fn(arr[i],i)){
+            arr2.push(arr[i]);
+        }
+    }
+    return arr2;
+};

ade4ffbf002729c5288449c8a616064c98b3d955
Pawan Kumar Sharma <pawansharma19feb@gmail.com>
2025-03-26 12:40:25 +0530

    Time: 2454 ms (5%), Space: 7.8 MB (18.85%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        for(long i=1;i<=num;i++){
+            if(i*i==(num)){
+                return true;
+            }
+        }
+        return false;
+        
+    }
+};
